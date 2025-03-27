4593728d0f98fbe1c407b0e18a3848a18d66ba46
AymanBj99 <ayman.bouajaji66@gmail.com>
2025-03-27 13:24:53 +0000

    refaire projet

--- app.py
+++ app.py
@@ -1,61 +1,29 @@
 from flask import Flask, request, jsonify
-from minio import Minio
-from dotenv import load_dotenv
-import fitz  # PyMuPDF
+from flask_cors import CORS
+from minio_utils import upload_file, create_bucket
 import os
 
-# Charger les variables d'environnement depuis le fichier .env
-load_dotenv()
-
 app = Flask(__name__)
+CORS(app)  # Autorise React à appeler Flask
 
-# Configuration de MinIO avec les variables d'environnement
-MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
-MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
-MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
-BUCKET_NAME = os.getenv("BUCKET_NAME")
-
-minio_client = Minio(
-    MINIO_ENDPOINT,
-    access_key=MINIO_ACCESS_KEY,
-    secret_key=MINIO_SECRET_KEY,
-    secure=False  # Désactiver SSL pour un usage local
-)
-
-#extraction des fichiers pdf 
-def extract_text_from_pdf(pdf_file_path):
-    doc = fitz.open(pdf_file_path)  # Ouvrir le fichier PDF
-    text = ""
-    
-    for page_num in range(doc.page_count):  # Parcourir chaque page du PDF
-        page = doc.load_page(page_num)  # Charger la page
-        text += page.get_text()  # Extraire le texte de la page
-    
-    return text
+# Créer le bucket au démarrage
+create_bucket()
 
 @app.route("/upload", methods=["POST"])
 def upload_cv():
     if "file" not in request.files:
         return jsonify({"error": "Aucun fichier trouvé"}), 400
-    
+
     file = request.files["file"]
     file_name = file.filename
-    
+
     try:
         # Upload vers MinIO
-        minio_client.put_object(
-            BUCKET_NAME,
-            file_name,
-            file,
-            length=-1,
-            part_size=10*1024*1024,
-            content_type="application/pdf"
-        )
+        upload_file(file, file_name)
+
         return jsonify({"message": f"Fichier {file_name} uploadé avec succès"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
-
 
 if __name__ == "__main__":
     app.run(debug=True)
