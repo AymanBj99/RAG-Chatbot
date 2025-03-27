9a553f143a5b77c3f24def6bdbe22e5e517ac267
AymanBj99 <ayman.bouajaji66@gmail.com>
2025-03-27 15:10:57 +0000

    changement de la fonction extract

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_cors import CORS
 from minio_utils import upload_file, create_bucket
 from qdrant_utils import create_collection, check_collection
 from pdf_utils import extract_text_from_pdf
+from embedding_utils import get_embedding
+from minio_utils import get_pdf_text
+
+
 
 
 import os
@@ -29,16 +33,30 @@ def upload_cv():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-@app.route("/extract-text", methods=["POST"])
-def extract_text():
-    if "file" not in request.files:
-        return jsonify({"error": "Aucun fichier trouvé"}), 400
+    
+@app.route("/get_pdf_text", methods=["POST"])
+def extract_pdf_text():
+    if "file_name" not in request.json:
+        return jsonify({"error": "Nom de fichier manquant"}), 400
 
-    file = request.files["file"]
+    file_name = request.json["file_name"]
     
     try:
-        text = extract_text_from_pdf(file)
+        text = get_pdf_text(file_name)
         return jsonify({"text": text}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/vectorize", methods=["POST"])
+def vectorize_text():
+    if "text" not in request.json:
+        return jsonify({"error": "Texte manquant"}), 400
+
+    text = request.json["text"]
+    
+    try:
+        vector = get_embedding(text).tolist()  # Convertir en liste pour JSON
+        return jsonify({"vector": vector}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
